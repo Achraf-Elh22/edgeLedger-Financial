5d89f3dfa2cf8cd92296b21c0241b20aa0b66f07
Achraf Elhamzaoui <achrfelh22@gmail.com>
2020-05-21 18:10:22 +0000

    make the website responsive for widscreens and tablets

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+// GOOGLE MAP API SETUP
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -5,3 +7,31 @@ function initMap() {
         zoom: 8
     });
 }
+
+//StickMenu Background
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+
+        document.getElementById("navbar").style.opacity = 1;
+    }
+});
+
+// Smooth Scrolling
+
+$('#navbar a, .btn').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
